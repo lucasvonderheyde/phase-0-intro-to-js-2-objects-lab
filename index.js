2e0d82a131ab769aad3036fcea07c1c55261b1a0
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-07-31 22:10:27 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "Lucas",
+    streetAddress: "11 and Broadway"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployeeObj = { ...employee};
+    
+    newEmployeeObj[key] = value;
+
+    return newEmployeeObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    console.log(employee)
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const anotherNewEmployee = {...employee};
+    
+    delete anotherNewEmployee[key];
+
+    return anotherNewEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   delete employee[key]
+   return employee
+}
